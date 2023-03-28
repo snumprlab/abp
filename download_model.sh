b068a6e29e619f405441a38d48f8b24ea0b6a511
Byeonghwi Kim <byeonghwik@allenai.org>
2023-03-28 23:22:53 +0900

    Update download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#echo "Downloading Pretrained MOCA Weight ..."
-#wget https://alfred-colorswap.s3.us-east-2.amazonaws.com/pretrained.pth
-#mkdir exp
-#mkdir exp/pretrained
-#mv pretrained.pth exp/pretrained
+
+# Unzip pretrained weight
+cat exp/pretrained/pretrained.tar.gza* | tar xzvf -
+rm exp/pretrained/pretrained.tar.gza*
+mv pretrained.pth exp/pretrained
 
 # we provide the pretrained mask rcnn
 wget https://alfred-colorswap.s3.us-east-2.amazonaws.com/weight_maskrcnn.pt
